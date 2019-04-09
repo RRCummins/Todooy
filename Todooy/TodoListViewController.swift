6cd23479bee972c3e2c9a11666563297e848995d
RRCummins <brokenrhino@gmail.com>
2019-04-09 19:45:07 -0400

    Add didSelevtRow functionality

--- Todooy/TodoListViewController.swift
+++ Todooy/TodoListViewController.swift
@@ -33,6 +33,24 @@ class TodoListViewController: UITableViewController {
     return cell
   }
   
+  //MARK - TableView Delegate Method
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+      tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    } else {
+      tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+    
+    
+    tableView.deselectRow(at: indexPath, animated: true)
+    
+    
+    
+    
+  }
+  
 
 }
 
