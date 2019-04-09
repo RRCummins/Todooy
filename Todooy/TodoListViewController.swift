464a8ca44b6d0b31f485e73f277649dd74c74d24
RRCummins <brokenrhino@gmail.com>
2019-04-09 00:28:00 -0400

    Add Icon Add Launch Image

--- Todooy/TodoListViewController.swift
+++ Todooy/TodoListViewController.swift
@@ -11,23 +11,26 @@ import UIKit
 class TodoListViewController: UITableViewController {
   
   //MARK - Instance Variables
+  
   let itemArray = ["Find Mike", "Buy Eggod", "Destroy Demogorgon"]
   
   //MARK - viewDidLoad
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
   //MARK - tableView Datasource Methods
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return itemArray.count
+  }
+  
+  
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
     cell.textLabel?.text = itemArray[indexPath.row]
     return cell
-  }
-  
-  
-  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return itemArray.count
   }
   
 
