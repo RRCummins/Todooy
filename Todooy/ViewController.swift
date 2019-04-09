b4ae5813f1365f06a044591917a2cb31c61a599b
RRCummins <brokenrhino@gmail.com>
2019-04-08 23:19:16 -0400

    Initial Commit

---
+++ Todooy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todooy
+//
+//  Created by Ryan Cummins on 4/8/19.
+//  Copyright © 2019 Them Rhinos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
