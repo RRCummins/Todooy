e3f1cc6427cb1284378a7955d842b6942dd26a11
RRCummins <brokenrhino@gmail.com>
2019-04-25 21:16:16 -0400

    Fixed URL casting

--- Todooy/AppDelegate.swift
+++ Todooy/AppDelegate.swift
@@ -18,8 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     
-    print(Realm.Configuration.defaultConfiguration.fileURL as Any)
-    //file:///Users/ryancummins/Library/Developer/CoreSimulator/Devices/777F5C19-57A4-484D-9ADD-A2473816870D/data/Containers/Data/Application/065CF636-D4FA-4008-98BF-2F34BF475206/Documents/
+    //    print(Realm.Configuration.defaultConfiguration.fileURL as Any)
+
 
     
     let data = Data()
